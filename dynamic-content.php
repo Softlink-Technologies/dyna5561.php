f6c737e2a89a91f8e8cf0aa7c6fa680adeaaddec
SACHIN <119405675+sachin-fullstack@users.noreply.github.com>
2023-03-08 15:56:45 +0530

    Create dynamic-content.php

---
+++ dynamic-content.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dynamic Web Page Example</title>
+</head>
+<body>
+
+	<h1>Welcome to my dynamic web page!</h1>
+
+	<?php
+	// Generate dynamic content here
+	$greeting = "Hello, world!";
+	echo "<p>" . $greeting . "</p>";
+	?>
+
+	<p>This is a dynamic web page that generates content using PHP.</p>
+
+</body>
+</html>
